f1b6589fe2c7cfd8ec9e4733f79290c4ca62181a
Christophe Delord <cdelord.fr>
2023-07-19 23:35:04 +0200

    load LuaX modules

--- fib.lua
+++ fib.lua
@@ -13,6 +13,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --]]
 
+local imath = require "imath"
+
 local one = imath.new(1)
 
 function naive_fib(n)
