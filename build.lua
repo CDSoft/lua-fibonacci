0ca63fb6d3e5f7e84c3ff0679fe92545d9076bfb
Christophe Delord <cdelord.fr>
2024-07-12 23:18:03 +0200

    use bang builders

--- build.lua
+++ build.lua
@@ -1,8 +1,8 @@
 var "builddir" ".build"
 
 local pdf = pipe {
-    rule "ypp.typ"   { command = "ypp -l fib --MF $depfile $in -o $out", depfile = "$builddir/$out.d" },
-    rule "typst.pdf" { command = "typst compile $in $out" },
+    build.ypp:new "ypp.typ" : add "flags" "-l fib" : set "depfile" "$builddir/$out.d",
+    build.typst,
 }
 
 pdf "fib.pdf" { "fib.typ" }
